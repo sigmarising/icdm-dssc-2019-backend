01d394206406757ec78f6f5328ceb7aebc16303a
Jason Zhang <sigmarising@hotmail.com>
2019-08-21 15:03:09 +0800

    ADD for production mode

--- icdm2019/settings.py
+++ icdm2019/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dn$e+mwb7qba@eatcywnio5ql68&$k$=b_4t5xbhbxzu2yp9!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # USE MEM CACHE
 CACHES = {
